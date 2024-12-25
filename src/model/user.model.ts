dd82207b07aec1581cd679aa57af3b8d2c7c89f1
Nikhil Pathak <nikhilpathak2602@gmail.com>
2024-12-25 18:44:41 +0530

    User Model

---
+++ src/model/user.model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema=new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true,
+        minLength:[6,"fullname should be atleast of 6 characters"]
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minLength:[5,"Email must be of 5 characters"]
+    },
+    date_of_birth:{
+        type:Date,
+        required:true
+    }
+},{
+    timestamps:true
+});
+
+const User=mongoose.model("user",UserSchema);
+export default User;
